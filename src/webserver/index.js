1c90f04040657521fb81ed8e27eea44556a21ca6
Taump <taump@yandex.ru>
2024-12-11 23:07:13 +0400

    feat: refactor webserver module to export fastify instance

--- src/webserver/index.js
+++ src/webserver/index.js
@@ -1,13 +1,9 @@
-const conf = require('ocore/conf.js');
 const fastify = require('fastify');
 const CORS = require('@fastify/cors');
 const fastifySensible = require('@fastify/sensible');
 
-const logger = require('../utils/logger');
-
 // Controllers
 const notFoundController = require('./controllers/notFoundController');
-// const verifyUrlController = require('./controllers/verifyUrlController');
 
 // Create instance
 const fastifyInstance = fastify({ logger: false });
@@ -23,11 +19,6 @@ fastifyInstance.register(fastifySensible);
 fastifyInstance.setNotFoundHandler(notFoundController);
 
 // Run the server
-module.exports = async (port) => {
-    try {
-        return await fastifyInstance.listen({ port: port ?? conf.webserverPort, host: '0.0.0.0' });
-    } catch (err) {
-        logger.error(err);
-        process.exit(1);
-    }
-}
+module.exports = {
+    fastifyInstance
+};
