ed3ac97a456873f4c46eada1a7484e3e9aa50db0
Taump <taump@yandex.ru>
2024-12-12 00:59:26 +0400

    fix: refactor pairedHandler to improve data handling and validation in pairing event

--- src/walletHandlers/pairedHandler.js
+++ src/walletHandlers/pairedHandler.js
@@ -3,7 +3,6 @@ const device = require('ocore/device');
 const mutex = require('ocore/mutex.js');
 
 const logger = require('../utils/logger');
-const toUpperCaseFirstLetter = require('../utils/toUpperCaseFirstLetter');
 
 const dictionary = require('../../dictionary');
 const DbService = require('../db/DbService');
@@ -13,13 +12,26 @@ eventBus.on('paired', async (from_address, data) => {
     logger.error('data', data);
 
     if (typeof data === 'string' && (data.match(/-/g) || []).length === 2) {
-        const [provider, address, user_id, encodedUsername] = data.split('-');
-        const username = decodeURIComponent(encodedUsername);
+        const [provider, address, dataString] = data.split('-');
+
+        let dataObject = {};
+
+        try {
+            const dataParams = new URLSearchParams(dataString);
+            dataObject = Object.fromEntries(dataParams.entries());
+
+            if (dataObject && !Validation.isDataObject(dataObject)) throw new Error('Invalid data object');
+        } catch (err) {
+            logger.error('Invalid data object:', err);
+            unlock("unknown error");
+            return device.sendMessageToDevice(from_address, 'text', dictionary.common.CANNOT_FIND_ORDER);
+        }
+
 
         let order;
 
         try {
-            order = await DbService.getAttestationOrders({ serviceProvider: provider, userId: user_id, username, address });
+            order = await DbService.getAttestationOrders({ serviceProvider: provider, data: dataObject, address });
 
             if (!order) {
                 unlock(dictionary.common.CANNOT_FIND_ORDER);
@@ -36,17 +48,11 @@ eventBus.on('paired', async (from_address, data) => {
             return device.sendMessageToDevice(from_address, 'text', dictionary.wallet.ORDER_ALREADY_ATTESTED);
         }
 
-        device.sendMessageToDevice(from_address, 'text', dictionary.wallet.ASK_VERIFY_FN(address, provider, user_id, encodeURIComponent(order.username)));
+        device.sendMessageToDevice(from_address, 'text', dictionary.wallet.ASK_VERIFY_FN(address, { provider, ...dataObject }));
         unlock();
-    } else if (typeof data === 'string') {
-        device.sendMessageToDevice(from_address, 'text', dictionary.wallet.PAIRING_WELCOME);
-
-        // const providers = Object.values(strategies).map(s => s.strategy);
-        // device.sendMessageToDevice(from_address, 'text', `Please select social network to attest: \n${providers.map((provider) => `➡ [${toUpperCaseFirstLetter(provider)}](command:attest_${provider})`).join('\n')} `);
-        unlock(dictionary.common.INVALID_DATA_FORMAT);
     } else {
         unlock(dictionary.common.INVALID_DATA_FORMAT);
         logger.error('Invalid data format received on pairing:', data);
-        return device.sendMessageToDevice(from_address, 'text', dictionary.common.INVALID_DATA_FORMAT);
+        return device.sendMessageToDevice(from_address, 'text', "Invalid data format");
     }
 });
