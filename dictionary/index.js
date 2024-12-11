62259c9bfe4f101a2fb1934f920d8cc6912a68c9
Taump <taump@yandex.ru>
2024-12-11 23:20:59 +0400

    feat: add setLocale function to dynamically update dictionary locales

--- dictionary/index.js
+++ dictionary/index.js
@@ -3,12 +3,23 @@
  * @module dictionary
  * @property {Object} wallet - Wallet-related messages and prompts
  * @property {Object} common - Common messages shared across the application
+ * @function addLocale - Add a new locale to the dictionary
  * @example
  * const { wallet, common } = require('./dictionary');
  * console.log(wallet.SIGN_MESSAGE_PROMPT);
  * console.log(common.INVALID_ADDRESS);
  */
-module.exports = {
+
+const locales = {
     wallet: require('./wallet.local'),
     common: require('./common.local'),
+}
+
+const setLocale = (provider, path) => {
+    locales[provider] = require(path);
+}
+
+module.exports = {
+    ...locales,
+    setLocale
 }
