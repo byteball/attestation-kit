471b2ad21841fc6fd0dbb3c8ca35a03e0ef2e66c
Taump <taump@yandex.ru>
2024-12-11 23:32:23 +0400

    fix: update setLocale function to load locales from the parent directory

--- dictionary/index.js
+++ dictionary/index.js
@@ -16,7 +16,7 @@ const locales = {
 }
 
 const setLocale = (provider, path) => {
-    locales[provider] = require(__dirname + path);
+    locales[provider] = require(__dirname + '../');
 }
 
 module.exports = {
