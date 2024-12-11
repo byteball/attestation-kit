bd4b5531caf70ae2090e8607e1b4d3b450ad0abd
Taump <taump@yandex.ru>
2024-12-11 23:00:07 +0400

    feat: add DbService to module exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     start: require("./start"),
     utils: require("./utils"),
+    db: require("./db/DbService"),
     dictionary: require("../dictionary"),
     BaseStrategy: require("./BaseStrategy"),
     webserver: require("./webserver"),
