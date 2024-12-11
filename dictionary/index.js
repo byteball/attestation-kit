519049079c131d490876b70a29b723bfc4960371
Taump <taump@yandex.ru>
2024-12-11 21:07:29 +0400

    refactor: add usage examples to dictionary module documentation

--- dictionary/index.js
+++ dictionary/index.js
@@ -3,6 +3,10 @@
  * @module dictionary
  * @property {Object} wallet - Wallet-related messages and prompts
  * @property {Object} common - Common messages shared across the application
+ * @example
+ * const { wallet, common } = require('./dictionary');
+ * console.log(wallet.SIGN_MESSAGE_PROMPT);
+ * console.log(common.INVALID_ADDRESS);
  */
 module.exports = {
     wallet: require('./wallet.local'),
