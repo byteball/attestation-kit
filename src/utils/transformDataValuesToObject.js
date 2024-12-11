1d4b4f10aa19ec9638adc0b59e23b7116aea95cd
Taump <taump@yandex.ru>
2024-12-12 02:18:48 +0400

    fix: add JSDoc comments to transformDataValuesToObject for better documentation and clarity

--- src/utils/transformDataValuesToObject.js
+++ src/utils/transformDataValuesToObject.js
@@ -1,3 +1,12 @@
+/**
+ * Transforms database data with numbered keys into a structured object.
+ * @param {Object} dbData - The database data object containing numbered keys (dataKey0, dataValue0, etc.)
+ * @returns {Object} An object mapping the keys to their corresponding values
+ * @throws {Error} If input is invalid or data structure is corrupted
+ * @example
+ * // Input: { dataKey0: 'name', dataValue0: 'John', dataKey1: 'age', dataValue1: '30' }
+ * // Output: { name: 'John', age: '30' }
+ */
 module.exports = (dbData) => { // { dataKey0, dataKey1, dataKey2, dataKey3, dataValue0, dataValue1, dataValue2, dataValue3 }
     const result = {};
 
@@ -9,7 +18,6 @@ module.exports = (dbData) => { // { dataKey0, dataKey1, dataKey2, dataKey3, data
         } else {
             break;
         }
-        
     }
 
     return result;
