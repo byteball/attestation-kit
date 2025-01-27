165db8288d607ea16baf53800bb4ef03ecbc0b2f
Taump <taump@yandex.ru>
2025-01-27 17:02:30 +0400

    feat: integrate DAG balance reading for attestor address

--- src/start.js
+++ src/start.js
@@ -1,3 +1,4 @@
+const dag = require('aabot/dag.js');
 const dbService = require('./db');
 const logger = require('./utils/logger');
 
@@ -6,6 +7,12 @@ module.exports = async (func = () => { }) => {
         logger.error('typeof func isn\'t a function', typeof func);
         throw new TypeError('Argument must be a function');
     }
+
+
+    const attestorAddress = await headlessWallet.readFirstAddress();
+    const balances = await dag.readBalance([attestorAddress]);
+
+    logger.info(`Attestor balance(${attestorAddress}):`, balances?.base);
 
     return new Promise((resolve, reject) => {
         require('./walletHandlers')(async () => {
