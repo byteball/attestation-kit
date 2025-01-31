e836ddbb64fbfe27241106cd209d214e0136d770
Taump <taump@yandex.ru>
2025-01-31 18:20:27 +0400

    fix: rename attestation entry handler to attestation start handler in message and paired handlers

--- src/walletHandlers/messageHandler.js
+++ src/walletHandlers/messageHandler.js
@@ -6,7 +6,7 @@ const verifyHandler = require("./verifyHandler");
 
 const walletAddressHandler = require('./walletAddressHandler');
 const walletSessionStore = require('./walletSessionStore');
-const attestationEntryHandler = require('./attestationEntryHandler');
+const attestationStartHandler = require('./attestationStartHandler');
 
 eventBus.on('text', async (device_address, data) => {
     const unlock = await mutex.lock(device_address);
@@ -16,7 +16,7 @@ eventBus.on('text', async (device_address, data) => {
         if (data.trim().startsWith("[Signed message]")) { // User send signed message
             await verifyHandler(device_address, data);
         } else if (data === "attest") {
-            await attestationEntryHandler(device_address);
+            await attestationStartHandler(device_address);
         } else if (session) {
             await walletAddressHandler(device_address, String(data).trim());
         } else {
--- src/walletHandlers/pairedHandler.js
+++ src/walletHandlers/pairedHandler.js
@@ -1,13 +1,13 @@
 const eventBus = require('ocore/event_bus.js');
 const mutex = require('ocore/mutex.js');
 
-const attestationEntryHandler = require('./attestationEntryHandler');
+const attestationStartHandler = require('./attestationStartHandler');
 
 eventBus.on('paired', async (device_address, data) => {
     if (data === 'back') return;
 
     const unlock = await mutex.lock(device_address);
-    await attestationEntryHandler(device_address, data);
+    await attestationStartHandler(device_address, data);
 
     return unlock();
 });
